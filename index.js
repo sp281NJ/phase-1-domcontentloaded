73583b83003263efd98f7522ca0bcad6f0ec7906
Stephen <pena731.junk@gmail.com>
2023-06-28 13:56:22 -0400

    DOMcontent loaded event

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    const domloaded = document.getElementById("text");
+   if (domloaded != " "){
+    //console.log(domloaded);
+
+   domloaded.textContent ="This is really cool!";
+}
+    else{
+      /*  console.log(
+        "This console.log() fires when index.js loads - before DOMContentLoaded is triggered"
+      );*/
+      domloaded.textContent ="shit - This console.log() fires when index.js loads - before DOMContentLoaded is triggered";}
+  });
+  
+  
